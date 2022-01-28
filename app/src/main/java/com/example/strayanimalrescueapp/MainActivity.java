2f238e0ca9b9c26ea5ca3ea1b1df9e887e6afe65
Kiew1005 <puikiew14@gmail.com>
2022-01-29 01:15:18 +0800

    Animal List

--- app/src/main/java/com/example/strayanimalrescueapp/MainActivity.java
+++ app/src/main/java/com/example/strayanimalrescueapp/MainActivity.java
@@ -4,55 +4,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
-    private TextView message;
-    private int counter = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        message = findViewById(R.id.clickCounter);
+
         ImageView droid = findViewById(R.id.stray);
 
-        //Define and attach click listener
-        droid.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tapDroid();
-            }
-        });
     }
-
-    private void tapDroid() {
-        counter++;
-        String countAsText;
-        /*
-         * In real applications you should not write switch like the one below.
-         * Use resource of type "Quantity strings (plurals)" instead.
-         * See https://developer.android.com/guide/topics/resources/string-resource#Plurals
-         */
-        switch (counter) {
-            case 1:
-                countAsText = "once";
-                break;
-            case 2:
-                countAsText = "twice";
-                break;
-            default:
-                countAsText = String.format("%d times", counter);
-        }
-        message.setText(String.format("You touched the text %s", countAsText));
-    }
-
 
     public void next(View v)
     {
-        Intent i = new Intent(this, Menu.class);
+        Intent i = new Intent(this, com.example.strayanimalrescueapp.ui.login.LoginActivity.class);
         startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/strayanimalrescueapp/ui/login/LoginActivity.java
@@ -0,0 +1,150 @@
+package  com.example.strayanimalrescueapp.ui.login;
+
+import android.app.Activity;
+import android.content.Intent;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.strayanimalrescueapp.Menu;
+import com.example.strayanimalrescueapp.databinding.ActivityLoginBinding;
+import com.example.strayanimalrescueapp.R;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    private LoginViewModel loginViewModel;
+    private ActivityLoginBinding binding;
+    TextView link_register;
+    EditText name;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        link_register = (TextView) findViewById(R.id.link_register);
+
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        loginViewModel = new ViewModelProvider(this, new LoginViewModelFactory())
+                .get(LoginViewModel.class);
+
+        name = (EditText) findViewById(R.id.name);
+        final String message = name.getText().toString();
+
+        final EditText usernameEditText = binding.username;
+        final EditText passwordEditText = binding.password;
+        final Button loginButton = binding.login;
+        final ProgressBar loadingProgressBar = binding.loading;
+
+        loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
+            @Override
+            public void onChanged(@Nullable LoginFormState loginFormState) {
+                if (loginFormState == null) {
+                    return;
+                }
+                loginButton.setEnabled(loginFormState.isDataValid());
+                if (loginFormState.getUsernameError() != null) {
+                    usernameEditText.setError(getString(loginFormState.getUsernameError()));
+                }
+                if (loginFormState.getPasswordError() != null) {
+                    passwordEditText.setError(getString(loginFormState.getPasswordError()));
+                }
+            }
+        });
+
+        loginViewModel.getLoginResult().observe(this, new Observer<LoginResult>() {
+            @Override
+            public void onChanged(@Nullable LoginResult loginResult) {
+                if (loginResult == null) {
+                    return;
+                }
+                loadingProgressBar.setVisibility(View.GONE);
+                if (loginResult.getError() != null) {
+                    showLoginFailed(loginResult.getError());
+                }
+                if (loginResult.getSuccess() != null) {
+                    updateUiWithUser(loginResult.getSuccess());
+                }
+                setResult(Activity.RESULT_OK);
+
+                //Complete and destroy login activity once successful
+                finish();
+            }
+        });
+
+        TextWatcher afterTextChangedListener = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // ignore
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // ignore
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                loginViewModel.loginDataChanged(usernameEditText.getText().toString(),
+                        passwordEditText.getText().toString());
+            }
+        };
+        usernameEditText.addTextChangedListener(afterTextChangedListener);
+        passwordEditText.addTextChangedListener(afterTextChangedListener);
+        passwordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    loginViewModel.login(usernameEditText.getText().toString(),
+                            passwordEditText.getText().toString(), name.getText().toString());
+                }
+                return false;
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadingProgressBar.setVisibility(View.VISIBLE);
+                loginViewModel.login(usernameEditText.getText().toString(),
+                        passwordEditText.getText().toString(), name.getText().toString());
+            }
+        });
+    }
+    public void nextActivity1(View v)
+    {
+        Intent i = new Intent(this, RegisterActivity.class);
+        startActivity(i);
+    }
+
+    private void updateUiWithUser(LoggedInUserView model) {
+        String welcome = getString(R.string.welcome) + model.getDisplayName(name.getText().toString());
+        // TODO : initiate successful logged in experience
+        Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
+        Intent i = new Intent(this, Menu.class);
+        startActivity(i);
+    }
+
+    private void showLoginFailed(@StringRes Integer errorString) {
+        String fail = getString(R.string.login_failed);
+        Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
+
+    }
+
+}
