9542e1d3979bd364d427ff82f802baf470da0b6c
Chieng Xing Tien <xingtien@student.usm.my>
2022-01-28 12:38:16 +0800

    StrayAnimalsInfo

--- app/src/main/java/com/example/strayanimalrescueapp/userchoice.java
+++ app/src/main/java/com/example/strayanimalrescueapp/userchoice.java
@@ -3,6 +3,7 @@ package com.example.strayanimalrescueapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class userchoice extends AppCompatActivity {
@@ -11,6 +12,7 @@ public class userchoice extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_userchoice);
+
     }
 
     public void choice1(View v)
@@ -29,4 +31,11 @@ public class userchoice extends AppCompatActivity {
         Intent i = new Intent(this, Report.class);
         startActivity(i);
     }
+
+    public void choiceViewInfo(View v)
+    {
+        Intent i = new Intent(this, StrayAnimals.class);
+        startActivity(i);
+    }
+
 }
